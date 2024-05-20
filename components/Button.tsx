b8bcc94d6459c1e1a4ec3c9a78fed227d1aaa241
Natnael Demelash <natidemelash074@gmail.com>
2024-05-20 21:19:49 +0300

    responsive home page hero section

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,7 +4,7 @@ type ButtonProps = {
   type: 'button' | 'submit';
   icon?: string;
   label: string;
-  variant: 'btn_dark_green';
+  variant: 'btn_dark_green' | 'btn_green' | 'btn_white_text';
 };
 
 const Button = ({ type, icon, label, variant }: ButtonProps) => {
